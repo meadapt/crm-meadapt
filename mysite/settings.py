390b871a3f66bb2b6a59b8614f9c9d6f64db29fd
Raiane Cardoso <raianecardoso.rede@gmail.com>
2025-01-20 18:54:08 -0300

    Inicia tutorial render

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pa@*nk0k5(^$6%3fqmc#_9=i1%&unf(g5^(xrrkh9i3&!pxjp@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,12 +79,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+if DEBUG == True: 
+
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+    }
+else:
+    DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string. 
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+    }
+
 
 
 # Password validation

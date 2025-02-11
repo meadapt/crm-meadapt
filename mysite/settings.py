3fbcbebb010da2f32b1bc16850efc1929b5581c5
Raiane Cardoso <raianecardoso.rede@gmail.com>
2025-02-11 20:30:43 -0300

    Add variáveis de ambiente no .env_exemplo e tirei do settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa@*nk0k5(^$6%3fqmc#_9=i1%&unf(g5^(xrrkh9i3&!pxjp@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG')=='True' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
